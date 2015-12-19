46476ba452c1617ed830d7ef89ae13309b0fe786
TJ Draper <tj@buzzingpixel.com>
2015-12-18 23:19:22 -0600

    Add master layout

---
+++ system/user/content/index.md
@@ -0,0 +1,5 @@
+---
+Title: About BuzzingPixel
+SeoTitle: Web Development, ExpressionEngine Expertise, and General Web Knowlege
+SeoDescription: BuzzingPixel specializes in custom web development, add-ons for ExpressionEngine and Craft, and custom web development
+---
---
+++ system/user/globals/nav.php
@@ -0,0 +1,181 @@
+<?php
+
+return [
+	'nav' => [
+		[
+			'title' => 'home',
+			'link' => '/',
+			'subnav' => []
+		],
+		[
+			'title' => 'news',
+			'link' => '/news',
+			'subnav' => []
+		],
+		[
+			'title' => 'ee add-ons',
+			'link' => '/ee-add-ons',
+			'subnav' => [
+				[
+					'title' => 'Collective',
+					'link' => '/ee-add-ons/collective',
+					'subnav' => [
+						[
+							'title' => 'about',
+							'link' => '/ee-add-ons/collective'
+						],
+						[
+							'title' => 'documentation',
+							'link' => '/ee-add-ons/collective/documentation'
+						],
+						[
+							'title' => 'changelog',
+							'link' => '/ee-add-ons/collective/changelog'
+						],
+						[
+							'title' => 'support',
+							'link' => '/ee-add-ons/collective/support'
+						]
+					]
+				],
+				[
+					'title' => 'Category Construct',
+					'link' => '/ee-add-ons/category-construct',
+					'subnav' => [
+						[
+							'title' => 'about',
+							'link' => '/ee-add-ons/category-construct'
+						],
+						[
+							'title' => 'documentation',
+							'link' => '/ee-add-ons/category-construct/documentation'
+						],
+						[
+							'title' => 'changelog',
+							'link' => '/ee-add-ons/category-construct/changelog'
+						],
+						[
+							'title' => 'support',
+							'link' => '/ee-add-ons/category-construct/support'
+						]
+					]
+				],
+				[
+					'title' => 'Mandrill Mailer',
+					'link' => '/ee-add-ons/mandrill-mailer',
+					'subnav' => [
+						[
+							'title' => 'about',
+							'link' => '/ee-add-ons/mandrill-mailer'
+						],
+						[
+							'title' => 'documentation',
+							'link' => '/ee-add-ons/mandrill-mailer/documentation'
+						],
+						[
+							'title' => 'changelog',
+							'link' => '/ee-add-ons/mandrill-mailer/changelog'
+						],
+						[
+							'title' => 'support',
+							'link' => '/ee-add-ons/mandrill-mailer/support'
+						]
+					]
+				],
+				[
+					'title' => 'Typographee',
+					'link' => '/ee-add-ons/typographee',
+					'subnav' => [
+						[
+							'title' => 'about',
+							'link' => '/ee-add-ons/typographee'
+						],
+						[
+							'title' => 'documentation',
+							'link' => '/ee-add-ons/typographee/documentation'
+						],
+						[
+							'title' => 'changelog',
+							'link' => '/ee-add-ons/typographee/changelog'
+						],
+						[
+							'title' => 'support',
+							'link' => '/ee-add-ons/typographee/support'
+						]
+					]
+				],
+				[
+					'title' => 'Construct',
+					'link' => '/ee-add-ons/construct',
+					'subnav' => [
+						[
+							'title' => 'about',
+							'link' => '/ee-add-ons/construct'
+						],
+						[
+							'title' => 'documentation',
+							'link' => '/ee-add-ons/construct/documentation'
+						],
+						[
+							'title' => 'changelog',
+							'link' => '/ee-add-ons/construct/changelog'
+						],
+						[
+							'title' => 'support',
+							'link' => '/ee-add-ons/construct/support'
+						]
+					]
+				],
+				[
+					'title' => 'Last Edit Date',
+					'link' => '/ee-add-ons/last-edit-date',
+					'subnav' => [
+						[
+							'title' => 'about',
+							'link' => '/ee-add-ons/last-edit-date'
+						],
+						[
+							'title' => 'documentation',
+							'link' => '/ee-add-ons/last-edit-date/documentation'
+						],
+						[
+							'title' => 'changelog',
+							'link' => '/ee-add-ons/last-edit-date/changelog'
+						],
+						[
+							'title' => 'support',
+							'link' => '/ee-add-ons/last-edit-date/support'
+						]
+					]
+				],
+				[
+					'title' => 'Pillster',
+					'link' => '/ee-add-ons/pillster',
+					'subnav' => [
+						[
+							'title' => 'about',
+							'link' => '/ee-add-ons/pillster'
+						],
+						[
+							'title' => 'documentation',
+							'link' => '/ee-add-ons/pillster/documentation'
+						],
+						[
+							'title' => 'changelog',
+							'link' => '/ee-add-ons/pillster/changelog'
+						],
+						[
+							'title' => 'support',
+							'link' => '/ee-add-ons/pillster/support'
+						]
+					]
+				]
+			]
+		],
+		[
+			'title' => 'contact',
+			'link' => '/contact',
+			'subnav' => []
+		]
+	]
+];
