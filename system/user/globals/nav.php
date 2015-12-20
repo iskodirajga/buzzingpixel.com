c0a9c448fd864ccf21b553958582c15eaf01b218
TJ Draper <tj@buzzingpixel.com>
2015-12-19 22:26:50 -0600

    Refactor Nav global

--- system/user/globals/nav.php
+++ system/user/globals/nav.php
@@ -2,169 +2,169 @@
 
 return [
 	'nav' => [
-		[
+		'home' => [
 			'title' => 'home',
 			'link' => '/',
 			'subnav' => []
 		],
-		[
+		'news' => [
 			'title' => 'news',
 			'link' => '/news',
 			'subnav' => []
 		],
-		[
+		'ee-add-ons' =>[
 			'title' => 'ee add-ons',
 			'link' => '/ee-add-ons',
 			'subnav' => [
-				[
+				'collective' => [
 					'title' => 'Collective',
 					'link' => '/ee-add-ons/collective',
 					'subnav' => [
-						[
+						'about' => [
 							'title' => 'about',
 							'link' => '/ee-add-ons/collective'
 						],
-						[
+						'documentation' => [
 							'title' => 'documentation',
 							'link' => '/ee-add-ons/collective/documentation'
 						],
-						[
+						'changelog' => [
 							'title' => 'changelog',
 							'link' => '/ee-add-ons/collective/changelog'
 						],
-						[
+						'support' => [
 							'title' => 'support',
 							'link' => '/ee-add-ons/collective/support'
 						]
 					]
 				],
-				[
+				'category-construct' => [
 					'title' => 'Category Construct',
 					'link' => '/ee-add-ons/category-construct',
 					'subnav' => [
-						[
+						'about' => [
 							'title' => 'about',
 							'link' => '/ee-add-ons/category-construct'
 						],
-						[
+						'documentation' => [
 							'title' => 'documentation',
 							'link' => '/ee-add-ons/category-construct/documentation'
 						],
-						[
+						'changelog' => [
 							'title' => 'changelog',
 							'link' => '/ee-add-ons/category-construct/changelog'
 						],
-						[
+						'support' => [
 							'title' => 'support',
 							'link' => '/ee-add-ons/category-construct/support'
 						]
 					]
 				],
-				[
+				'mandrill-mailer' => [
 					'title' => 'Mandrill Mailer',
 					'link' => '/ee-add-ons/mandrill-mailer',
 					'subnav' => [
-						[
+						'about' => [
 							'title' => 'about',
 							'link' => '/ee-add-ons/mandrill-mailer'
 						],
-						[
+						'documentation' => [
 							'title' => 'documentation',
 							'link' => '/ee-add-ons/mandrill-mailer/documentation'
 						],
-						[
+						'changelog' => [
 							'title' => 'changelog',
 							'link' => '/ee-add-ons/mandrill-mailer/changelog'
 						],
-						[
+						'support' => [
 							'title' => 'support',
 							'link' => '/ee-add-ons/mandrill-mailer/support'
 						]
 					]
 				],
-				[
+				'typographee' => [
 					'title' => 'Typographee',
 					'link' => '/ee-add-ons/typographee',
 					'subnav' => [
-						[
+						'about' => [
 							'title' => 'about',
 							'link' => '/ee-add-ons/typographee'
 						],
-						[
+						'documentation' => [
 							'title' => 'documentation',
 							'link' => '/ee-add-ons/typographee/documentation'
 						],
-						[
+						'changelog' => [
 							'title' => 'changelog',
 							'link' => '/ee-add-ons/typographee/changelog'
 						],
-						[
+						'support' => [
 							'title' => 'support',
 							'link' => '/ee-add-ons/typographee/support'
 						]
 					]
 				],
-				[
+				'construct' => [
 					'title' => 'Construct',
 					'link' => '/ee-add-ons/construct',
 					'subnav' => [
-						[
+						'about' => [
 							'title' => 'about',
 							'link' => '/ee-add-ons/construct'
 						],
-						[
+						'documentation' => [
 							'title' => 'documentation',
 							'link' => '/ee-add-ons/construct/documentation'
 						],
-						[
+						'changelog' => [
 							'title' => 'changelog',
 							'link' => '/ee-add-ons/construct/changelog'
 						],
-						[
+						'support' => [
 							'title' => 'support',
 							'link' => '/ee-add-ons/construct/support'
 						]
 					]
 				],
-				[
+				'last-edit-date' => [
 					'title' => 'Last Edit Date',
 					'link' => '/ee-add-ons/last-edit-date',
 					'subnav' => [
-						[
+						'about' => [
 							'title' => 'about',
 							'link' => '/ee-add-ons/last-edit-date'
 						],
-						[
+						'documentation' => [
 							'title' => 'documentation',
 							'link' => '/ee-add-ons/last-edit-date/documentation'
 						],
-						[
+						'changelog' => [
 							'title' => 'changelog',
 							'link' => '/ee-add-ons/last-edit-date/changelog'
 						],
-						[
+						'support' => [
 							'title' => 'support',
 							'link' => '/ee-add-ons/last-edit-date/support'
 						]
 					]
 				],
-				[
+				'pillster' => [
 					'title' => 'Pillster',
 					'link' => '/ee-add-ons/pillster',
 					'subnav' => [
-						[
+						'about' => [
 							'title' => 'about',
 							'link' => '/ee-add-ons/pillster'
 						],
-						[
+						'documentation' => [
 							'title' => 'documentation',
 							'link' => '/ee-add-ons/pillster/documentation'
 						],
-						[
+						'changelog' => [
 							'title' => 'changelog',
 							'link' => '/ee-add-ons/pillster/changelog'
 						],
-						[
+						'support' => [
 							'title' => 'support',
 							'link' => '/ee-add-ons/pillster/support'
 						]
@@ -172,7 +172,7 @@ return [
 				]
 			]
 		],
-		[
+		'contact' => [
 			'title' => 'contact',
 			'link' => '/contact',
 			'subnav' => []
