20d2742c8e7973f27c3ea18b6eadca3d265f9dd6
TJ Draper <tj@buzzingpixel.com>
2016-01-06 09:16:52 -0600

    Update backup script for staging

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -4,13 +4,13 @@
 DATE=$(date +"%Y-%m-%d__%H-%M-%S");
 
 # Make the backup directory based on the date
-mkdir backups/${DATE};
+mkdir /home/forge/staging.buzzingpixel.com/current/scripts/backups/${DATE};
 
 # Archive files
-tar czf backups/${DATE}/files.tar.gz ../ --exclude=scripts/backups;
+tar czf /home/forge/staging.buzzingpixel.com/current/scripts/backups/${DATE}/files.tar.gz /home/forge/staging.buzzingpixel.com/current/ --exclude=scripts/backups;
 
 # Sync to S3
-/usr/local/aws/bin/aws s3 sync backups s3://zoebackups
+/usr/local/aws/bin/aws s3 sync /home/forge/staging.buzzingpixel.com/current/scripts/backups/ s3://zoebackups
 
 # Delete local backup
-rm -rf backups/${DATE};
+rm -rf /home/forge/staging.buzzingpixel.com/current/scripts/backups/${DATE};
