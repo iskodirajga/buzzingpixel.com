ef1683f3fba8b2159d2cf3f261027464bd9f4bef
TJ Draper <tj@buzzingpixel.com>
2016-01-06 23:12:43 -0600

    Update after.sh to install xdebug in homestead environment

--- after.sh
+++ after.sh
@@ -4,11 +4,28 @@
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
 
+# Install Xdebug
+git clone git://github.com/xdebug/xdebug.git
+cd xdebug
+phpize
+./configure --enable-xdebug
+make
+make install
+
+# Configure Xdebug
+cat > /etc/php/mods-available/xdebug.ini <<EOL
+zend_extension=xdebug.so
+xdebug.default_enable=1
+xdebug.remote_enable=1
+xdebug.remote_port=9000
+xdebug.remote_autostart=1
+xdebug.remote_connect_back=1
+EOL
+ln -s /etc/php/mods-available/xdebug.ini /etc/php/7.0/fpm/conf.d/20-xdebug.ini
+service php7.0-fpm restart
+
 # Copy the nginx config
 sudo cp /home/vagrant/BuzzingPixel/nginxConf/local.conf /etc/nginx/sites-available/buzzingpixel.dev;
-
-# Copy PHP config
-sudo cp /home/vagrant/BuzzingPixel/nginxConf/localPhp.ini /etc/php/7.0/fpm/php.ini;
 
 # Copy SSL Certs
 sudo cp /home/vagrant/BuzzingPixel/devCerts/buzzingpixel.dev.crt /etc/nginx/ssl/buzzingpixel.dev.crt;
