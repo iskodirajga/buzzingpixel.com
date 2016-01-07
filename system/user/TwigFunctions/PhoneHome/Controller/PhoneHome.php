bb5b71f6fa086f51fdeb7ba9811c099f5056b393
TJ Draper <tj@buzzingpixel.com>
2016-01-06 20:30:53 -0600

    Add Phone Home functionality

--- system/user/TwigFunctions/PhoneHome/Controller/PhoneHome.php
+++ system/user/TwigFunctions/PhoneHome/Controller/PhoneHome.php
@@ -18,7 +18,51 @@ class PhoneHome
 	 */
 	public function run()
 	{
-		var_dump('here');
-		die;
+		$dataPath = realpath(dirname(__FILE__)) . '/../Data/';
+
+		// Make sure required params are in place
+		if (
+			! isset($_POST['app']) or
+			! $_POST['app'] or
+			! isset($_POST['domain']) or
+			! $_POST['domain']
+		) {
+			return;
+		}
+
+		$licenseExists = isset($_POST['license']) && $_POST['license'];
+		$license = $licenseExists ? $_POST['license'] : 'no-license';
+		$appPath = $dataPath . $_POST['app'];
+		$licensePath = $appPath . '/' . $license;
+		$file = $licensePath . '/' . $_POST['domain'] . '.md';
+
+		if (! is_dir($appPath)) {
+			mkdir($appPath);
+		}
+
+		if (! is_dir($licensePath)) {
+			mkdir($licensePath);
+		}
+
+		if ($licenseExists && is_dir($appPath . '/no-license')) {
+			$files = array_diff(scandir($appPath . '/no-license'), array('.', '..'));
+
+			foreach ($files as $file) {
+				unlink($appPath . '/no-license/' . $file);
+			}
+
+			rmdir($appPath . '/no-license');
+		}
+
+		file_put_contents(
+			$file,
+			"---
+App: {$_POST['app']}
+Domain: {$_POST['domain']}
+License: {$license}
+---
+
+"
+		);
 	}
 }
